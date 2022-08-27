8c22a0f357f8c26dc90a5cff2b9eb49e7a87bbc5
Rafael Castillo <rafatoski@hotmail.com>
2022-08-27 16:26:13 -0500

    Animaciónes de reveal y some fixes
    - se corrigío el espacio del footer
    - ase agrego el fondo de los heros de las paginas internas
    - se agrego la animacion de entrada de elementos

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
-const btnSwitch = document.querySelector('#switch');
-
-btnSwitch.addEventListener('click' , () => {
-    document.body.classList.toggle('light');
-    btnSwitch.classList.toggle('active');
-
-    //localstorage
-    if (document.body.classList.contains('light')){
-        localStorage.setItem('light-mode' , 'true');
+window.onload = function(){
+    //hide the preloader
+    document.querySelector(".preloader").style.display = "none";
+    //trigger wow animation
+    new WOW().init();
+    // dark light mode 
+    const btnSwitch = document.querySelector('#switch');
+    
+    btnSwitch.addEventListener('click' , () => {
+        document.body.classList.toggle('light');
+        btnSwitch.classList.toggle('active');
+    
+        //localstorage
+        if (document.body.classList.contains('light')){
+            localStorage.setItem('light-mode' , 'true');
+        }else {
+            localStorage.setItem('light-mode' , 'false');       
+        }
+    });
+    // modo actual
+    if(localStorage.getItem('light-mode') === 'true'){
+        document.body.classList.add('light');
+        btnSwitch.classList.add('active');
+    
     }else {
-        localStorage.setItem('light-mode' , 'false');       
+        document.body.classList.remove('light');
+        btnSwitch.classList.remove('active');
     }
-});
-// modo actual
-if(localStorage.getItem('light-mode') === 'true'){
-    document.body.classList.add('light');
-    btnSwitch.classList.add('active');
-
-}else {
-    document.body.classList.remove('light');
-    btnSwitch.classList.remove('active');
+    
 }
